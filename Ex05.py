9c32539a0cce5c3ddd9c9bd10588c613cea9970a
AnastasiaGainulina <109802756+AnastasiaGainulina@users.noreply.github.com>
2022-11-08 18:17:20 +0300

    Create Ex05.py

---
+++ Ex05.py
@@ -0,0 +1,18 @@
+
+# 5) Даны два файла, в каждом из которых находится запись многочлена. 
+# Задача - сформировать файл, содержащий сумму многочленов.
+
+with open('List_01.txt','r') as file:
+    poly_1 = file.readline()
+    list_of_poly_1 = poly_1.split()
+
+
+with open('List_02.txt','r') as file:
+    poly_2 = file.readline()
+    list_of_poly_2 = poly_2.split()
+
+print(f'{list_of_poly_1} + {list_of_poly_2}')
+sum_poly = list_of_poly_1 + list_of_poly_2
+
+with open('sum_poly.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{list_of_poly_1} + {list_of_poly_2}')
